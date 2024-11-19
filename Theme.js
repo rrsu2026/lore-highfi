5471347791267124fa13cd346f253ae8145ab645
Eliza <ebazakas@mit.edu>
2024-11-18 22:16:09 -0800

    Added Theme document

---
+++ Theme.js
@@ -0,0 +1,29 @@
+export default {
+    colors: {
+        // Blues
+        primaryColorBg: "#ecf1f8",
+        primaryColor1: rgb(164, 187, 223),
+        primaryColor2: rgb(113, 145, 195),
+        primaryColor3: rgb(75, 110, 166),
+        primaryColor4: rgb(50, 88, 148),
+        primaryColor5: rgb(28, 65, 124),
+
+        // Yellows
+        complementColorBg: "#fff6e5",
+        complementColor1: rgb(255, 228, 180),
+        complementColor2: rgb(255, 212, 137),
+        complementColor3: rgb(251, 196, 99),
+        complementColor4: rgb(224, 165, 61),
+        complementColor5: rgb(187, 129, 28),
+    },
+
+    fontFamily: "Montserrat", sansSerif, 
+
+    sizes: {
+      textHeading: 54,
+      textSubHeading: 36,
+
+      textParagraph: 32,
+      textMetadata: 20,
+    },
+}
