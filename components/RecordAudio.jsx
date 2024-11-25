7a5db394cc60dfa338d1781d31e18fc0d24001b9
rrsu2026 <rrsu@cs.stanford.edu>
2024-11-24 18:39:29 -0800

    RecordVideo and RecordAudio styling

--- components/RecordAudio.jsx
+++ components/RecordAudio.jsx
@@ -1,17 +1,64 @@
-import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, Pressable } from "react-native";
+import theme from "../Theme";
+import Entypo from "@expo/vector-icons/Entypo";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
-const RecordAudio = ({navigation}) => {
+const RecordAudio = ({ navigation }) => {
   return (
-    <View>
-      <Text>Recording...</Text>
-      <Button title="Pause" />
-      <Button title="Save" onPress={() => navigation.navigate("EditMetadata")} />
+    <View style={styles.container}>
+      <Text style={styles.headerText}>Recording...</Text>
+      <View style={styles.pauseCont}>
+        <Pressable style={styles.choiceButton}>
+          <Text style={styles.buttonText}>Pause</Text>
+          <FontAwesome name="pause" size={30} color="black" />
+        </Pressable>
+        <Pressable
+          style={styles.choiceButton}
+          onPress={() => navigation.navigate("EditMetadata")}
+        >
+          <Text style={styles.buttonText}>Save</Text>
+          <Entypo name="save" size={35} color="black" />
+        </Pressable>
       </View>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 10,
+  },
+  pauseCont: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    gap: 15,
+  },
+  choiceButton: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: theme.colors.complementColor2,
+    paddingVertical: 12,
+    paddingHorizontal: "2%",
+    borderRadius: 8,
+    borderWidth: 2,
+    borderColor: "#000",
+    marginBottom: 15,
+    gap: "10%",
+  },
+  headerText: {
+    fontSize: 24,
+    textAlign: "center",
+    marginHorizontal: "5%",
+    paddingHorizontal: "10%",
+    paddingVertical: "5%",
+  },
+  buttonText: {
+    fontSize: 24,
+  },
 });
 
 export default RecordAudio;
