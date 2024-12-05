1560daf4761180f84f529cae8ab8bb0aa6582818
AlurienFlame <62194841+AlurienFlame@users.noreply.github.com>
2024-12-04 22:55:04 -0800

    audio playing

--- components/StoryAudio.jsx
+++ components/StoryAudio.jsx
@@ -1,15 +1,38 @@
-import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Button } from 'react-native';
+import { Audio } from "expo-av";
 
 const StoryAudio = ({ audio }) => {
+  const [sound, setSound] = useState();
+
+  useEffect(() => {
+    return sound
+      ? () => {
+          console.log("Unloading Sound");
+          sound.unloadAsync();
+        }
+      : undefined;
+  }, [sound]);
+
+  async function playSound() {
+    try {
+      console.log("Loading sound...");
+      const { sound: newSound } = await Audio.Sound.createAsync({
+        uri: audio,
+      });
+      setSound(newSound);
+      console.log("Sound successfully set:", newSound);
+      await newSound.playAsync();
+    } catch (error) {
+      console.error("Error loading or playing sound:", error);
+    }
+  }
+  
   return (
     <View>
-      <Text>Audio Playback Interface Goes Here</Text>
+      <Button title="Play Audio" onPress={playSound} />
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-});
 
 export default StoryAudio;
